1a5cf22343e608d7ad7901b083b8727f9ea27b81
SpectralPixel <encrypteddvjjrxv@gmail.com>
2024-04-05 15:55:27 +0200

    Added i3 config There are a lot of files here because I made some custom scripts for i3, which require a virtual python environment on arch.

---
+++ .config/i3/py/per-workspace-opener.py
@@ -0,0 +1,29 @@
+import i3
+import os
+
+cur_workspace = 0
+
+workspaces = i3.get_workspaces()
+for ws in workspaces:
+    if ws['focused'] == True:
+        cur_workspace = ws['num']
+
+match cur_workspace:
+    case 1:
+        program_to_run = "firefox"
+    case 4:
+        program_to_run = "nautilus"
+    case 5:
+        program_to_run = "betterbird"
+    case 7:
+        program_to_run = "obs"
+    case 8:
+        program_to_run = "spotify"
+    case 9:
+        program_to_run = "vesktop"
+    case 10:
+        program_to_run = "flatpak run com.valvesoftware.Steam"
+    case _:
+        program_to_run = "kitty"
+
+os.system(f"i3-msg exec {program_to_run}")
