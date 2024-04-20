f4d372ee44112a793523718901378aa2c95203cf
SpectralPixel <encrypteddvjjrxv@gmail.com>
2024-04-20 19:55:27 +0200

    Made pushing silent (hopefully)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    
+
     # stop that pesky welcome message from showing it's face
     set fish_greeting
 
@@ -12,14 +12,14 @@ if status is-interactive
     alias restart='systemctl reboot'
     alias hx='helix'
     alias catsay='cowsay -f sillycat'
-    
+
     alias gprep='git add --all; git status'
-    alias gsend='git commit && git push'
+    alias gsend='git commit && git push >/dev/null && echo "Commited and pushed succesfully!"'
     alias gstat='git status'
     alias gcom='git commit'
     alias gadd='git add'
     alias gstow='gprep; gcom; stow --adopt .'
-    
+
     alias sysup='flatpak update && yay'
     alias sysclean='pacman -Qtdq | sudo pacman -Rns -'
     alias sps='sudo pacman -S'
@@ -28,20 +28,20 @@ if status is-interactive
     alias rst='cd && cls'
 
     function consoledeco
-        echo ------- Welcome to Arch Linux! -------                                                                                                                                                               
-        echo                                                                                                                                                                                                      
-        fastfetch                                                                                                                                                                                                 
-        echo TODO:                                                                                                                                                                                                
-        echo '- Make GRUB pretty'                                                                                                                                                                                 
-        echo '- Stop bluetooth from cutting out' 
+        echo ------- Welcome to Arch Linux! -------
+        echo
+        fastfetch
+        echo TODO:
+        echo '- Make GRUB pretty'
+        echo '- Stop bluetooth from cutting out'
     end
 
     # launch zoxide (cd replacement) and overwrite cd with an alias
     eval "$(zoxide init --cmd cd fish)"
-    
+
     # Custom environment variables (PATH)
     ./.env-var-setter
-    
+
     # custom lil text (and neofetch of course)
     consoledeco
 end
